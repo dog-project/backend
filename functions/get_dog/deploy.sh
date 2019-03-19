5bdc25df2948c98c233221db53f1effc8f57391e
Julian Zucker <julian.zucker@gmail.com>
2019-03-18 23:37:57 -0400

    Deleted deploy script copy
    Co-authored-by: Julian Zucker <julian.zucker@gmail.com>
    Co-authored-by: Daniel Rassaby <drassaby@gmail.com>

--- functions/get_dog/deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud functions deploy function-1 \
-  --project dog-project-234515 \
-  --source  https://source.developers.google.com/projects/dog-project-234515/repos/github_dog-project_backend/moveable-aliases/master/paths/function-1 \
-  --runtime python37 \
-  --trigger-http
