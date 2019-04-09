3d16946270705858db7cf20b1479401bbbd32488
Julian Zucker <julian.zucker@gmail.com>
2019-04-09 16:14:09 -0400

    use consistent delta in elo

--- functions/util/elo.py
+++ functions/util/elo.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+
 def compute_elo(results):
     """
     :param results: a list of (id1, id2, outcome) tuples, where outcome is one of "win", "loss", or "tie"
@@ -7,6 +11,8 @@ def compute_elo(results):
     # some dispute, see https://en.wikipedia.org/wiki/Elo_rating_system#Most_accurate_K-factor
     # we choose a low k-factor, because a photo's cuteness is unlikely to change over time
     k = 10
+    results = [x for x in results]
+    random.shuffle(results)
 
     elo = {}
     for id1, id2, outcome in results:
@@ -16,7 +22,7 @@ def compute_elo(results):
 
         # This implements a ratio such that dogs with a difference in elo of 400
         # have an expected outcome of 10:1 win:loss.
-        expected_outcome = 1 / (1 + 10 ** ((rank2 - rank1) / 400))
+        expected_outcome = int(1 / (1 + 10 ** ((rank2 - rank1) / 400)) * 1000) / 1000
 
         outcome1 = None
         outcome2 = None
@@ -27,7 +33,12 @@ def compute_elo(results):
         if outcome == "tie":
             outcome1, outcome2 = (0.5, 0.5)
 
-        elo[id1] = rank1 + k * (outcome1 - expected_outcome)
-        elo[id2] = rank2 + k * (outcome2 - expected_outcome)
+        delta = k * (outcome1 - expected_outcome)
+        elo[id1] = rank1 + delta
+        elo[id2] = rank2 - delta
+        print(rank1, rank2, expected_outcome, outcome1, outcome2, elo[id1], elo[id2])
 
     return sorted([[id, elo_score] for id, elo_score in elo.items()], key=lambda r: r[1], reverse=True)
+
+print(sum([x[1] for x in compute_elo([(1, 2, "win"), (1, 3, "win"), (1, 4, "tie")] * 200)])
+      / 4)
