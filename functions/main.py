c1ee7d647f8c4622814fa0a84f200a1265fefac7
Filip Musial <fmusial@outlook.com>
2020-02-14 23:01:41 -0500

    move submit to main

--- functions/main.py
+++ functions/main.py
@@ -472,3 +472,52 @@ def _get_elo_ranking(conn):
     with conn.cursor() as cursor:
         cursor.execute("SELECT dog1_id, dog2_id, result from votes")
         return compute_elo(cursor)
+        
+@cloudfunction(
+    in_schema={
+        # This code assumes that tiers are arrays in the database (since we are using postgreSQL) and that we do not have a tiers table
+        # Since candidates are strings, I had all the demographics be passed as strings as well
+        "type": "object",
+        "properties": {
+            "state": {"type": "string"},
+            "age_above_18": {"type": "boolean"},
+            "eligible": {"type": "boolean"},
+            "race": {"type": "array", "items": {"type": "string"}},
+            "gender": {"oneOf": [{"type": "string"}, {"type": "null"}]},
+            "education": {"oneOf": [{"type": "string"}, {"type": "null"}]},
+            "age": {"oneOf": [{"type": "string"}, {"type": "null"}]},
+            "party": {"oneOf": [{"type": "string"}, {"type": "null"}]},
+            "lgbtq": {"oneOf": [{"type": "boolean"}, {"type": "null"}]},
+            "top_candidate": {"type": "string"},
+            "tier1": {"type": "array", "items": {"type":  "string"}},
+            "tier2": {"type": "array", "items": {"type":  "string"}},
+            "tier3": {"type": "array", "items": {"type":  "string"}},
+            "tier4": {"type": "array", "items": {"type":  "string"}},
+            "tier5": {"type": "array", "items": {"type":  "string"}},
+            "tier6": {"type": "array", "items": {"type":  "string"}}
+        },
+        "additionalProperties": False,
+        "minProperties": 10
+    },
+    out_schema={"type": "null"}
+    )
+
+def submit(request_json, conn):
+    return _submit(request_json, conn)
+
+def _submit(data, conn):
+    voter_id = uuid.uuid4().int
+
+    cursor = conn.cursor()
+
+    cursor.execute("""INSERT INTO voters
+    (id, state, age_above_18, eligible, race, gender, education, age, party, lgbtq) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+    (voter_id, data["state"], data["age_above_18"], data["eligible"], data["race"], data["gender"], data["education"], data["age"], data["party"], data["lgbtq"]))
+
+    cursor.execute("""INSERT INTO ballot 
+    (voter, top_candidate, tier1, tier2, tier3, tier4, tier5, tier6) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+    (voter_id, data["top_candidate"], data["tier1"], data["tier2"], data["tier3"], data["tier4"], data["tier5"], data["tier6"]))
+
+    return None
